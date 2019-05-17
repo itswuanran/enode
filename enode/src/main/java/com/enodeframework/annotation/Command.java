4ae388a13c9f21703496e30cb9c8cd4ed2777a4b
anruence <anruence@gmail.com>
2019-05-18 01:13:24 +0800

    add annotation support

---
+++ enode/src/main/java/com/enodeframework/annotation/Command.java
@@ -0,0 +1,22 @@
+package com.enodeframework.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+public @interface Command {
+
+    /**
+     * The value may indicate a suggestion for a logical component name,
+     * to be turned into a Spring bean in case of an autodetected component.
+     *
+     * @return the suggested component name, if any (or empty String otherwise)
+     */
+    String value() default "";
+}
