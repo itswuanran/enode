ce1b1c803694055c255d27fdea692eb27b41352d
anruence <anruence@gmail.com>
2019-07-09 18:26:02 +0800

    modify db

--- samples/bank-transfer/domain/src/main/java/com/enodeframework/samples/QueueProperties.java
+++ samples/bank-transfer/domain/src/main/java/com/enodeframework/samples/QueueProperties.java
@@ -10,9 +10,9 @@ public class QueueProperties {
 
     public static String EXCEPTION_TOPIC = "ExceptionSample";
 
-    public static String NAMESRVADDR = "127.0.0.1:9876";
+    public static String NAMESRVADDR = "p.anruence.com:9876";
 
-    public static String KAFKA_SERVER = "127.0.0.1:9092";
+    public static String KAFKA_SERVER = "p.anruence.com:9092";
 
     public static String EVENT_PRODUCER_GROUP = "EventProducerGroup";
 
