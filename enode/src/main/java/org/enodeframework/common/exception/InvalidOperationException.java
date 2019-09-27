d03b1b44891ded049c910cc40b30112d1d050cee
anruence <anruence@gmail.com>
2019-09-27 21:04:01 +0800

    refract exception

--- enode/src/main/java/org/enodeframework/common/exception/InvalidOperationException.java
+++ enode/src/main/java/org/enodeframework/common/exception/InvalidOperationException.java
@@ -1,6 +1,12 @@
 package org.enodeframework.common.exception;
 
+/**
+ * @author anruence@gmail.com
+ */
 public class InvalidOperationException extends ENodeRuntimeException {
+
+    private static final long serialVersionUID = 6201111149059858558L;
+
     public InvalidOperationException() {
         super();
     }
--- enode/src/main/java/org/enodeframework/common/exception/VertxRuntimeException.java
+++ enode/src/main/java/org/enodeframework/common/exception/VertxRuntimeException.java
@@ -3,7 +3,9 @@ package org.enodeframework.common.exception;
 /**
  * @author anruence@gmail.com
  */
-public class VertxRuntimeException extends RuntimeException {
+public class VertxRuntimeException extends ENodeRuntimeException {
+
+    private static final long serialVersionUID = -8275416439421473887L;
 
     public VertxRuntimeException() {
         super();
