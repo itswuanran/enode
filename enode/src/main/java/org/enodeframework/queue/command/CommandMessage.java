42099b1330079a613bb72b0b92812250ff992e64
anruence <anruence@gmail.com>
2020-08-10 22:44:30 +0800

    make test run

--- enode/src/main/java/org/enodeframework/queue/command/CommandMessage.java
+++ enode/src/main/java/org/enodeframework/queue/command/CommandMessage.java
@@ -1,23 +1,23 @@
 package org.enodeframework.queue.command;
 
+import java.io.Serializable;
 import java.net.InetSocketAddress;
 
 /**
  * @author anruence@gmail.com
  */
-public class CommandMessage {
-    private String commandData;
-
-    public InetSocketAddress getReplyAddress() {
-        return replyAddress;
-    }
-
-    public void setReplyAddress(InetSocketAddress replyAddress) {
-        this.replyAddress = replyAddress;
-    }
-
-    private InetSocketAddress replyAddress;
+public class CommandMessage implements Serializable {
     private String commandType;
+    private String commandData;
+    private InetSocketAddress replyAddress;
+
+    public String getCommandType() {
+        return commandType;
+    }
+
+    public void setCommandType(String commandType) {
+        this.commandType = commandType;
+    }
 
     public String getCommandData() {
         return commandData;
@@ -27,12 +27,11 @@ public class CommandMessage {
         this.commandData = commandData;
     }
 
-
-    public String getCommandType() {
-        return commandType;
+    public InetSocketAddress getReplyAddress() {
+        return replyAddress;
     }
 
-    public void setCommandType(String commandType) {
-        this.commandType = commandType;
+    public void setReplyAddress(InetSocketAddress replyAddress) {
+        this.replyAddress = replyAddress;
     }
 }
