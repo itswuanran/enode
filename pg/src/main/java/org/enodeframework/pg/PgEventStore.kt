35ee060a7e429bfeb540c57dc36017f351907752
anruence <anruence@gmail.com>
2020-11-07 17:29:45 +0800

    javadoc plugin

--- pg/src/main/java/org/enodeframework/pg/PgEventStore.kt
+++ pg/src/main/java/org/enodeframework/pg/PgEventStore.kt
@@ -10,9 +10,7 @@ import javax.sql.DataSource
 /**
  * @author anruence@gmail.com
  */
-class PgEventStore : JDBCEventStore {
-    constructor(dataSource: DataSource, eventSerializer: IEventSerializer, serializeService: ISerializeService) : super(dataSource, eventSerializer, serializeService) {}
-    constructor(dataSource: DataSource, setting: DBConfiguration, eventSerializer: IEventSerializer, serializeService: ISerializeService) : super(dataSource, setting, eventSerializer, serializeService) {}
+class PgEventStore(dataSource: DataSource, setting: DBConfiguration, eventSerializer: IEventSerializer, serializeService: ISerializeService) : JDBCEventStore(dataSource, setting, eventSerializer, serializeService) {
 
     public override fun parseDuplicateCommandId(msg: String): String {
         val matcher = PATTERN_POSTGRESQL.matcher(msg)
--- tidb/src/main/java/org/enodeframework/tidb/TiDBEventStore.kt
+++ tidb/src/main/java/org/enodeframework/tidb/TiDBEventStore.kt
@@ -10,9 +10,7 @@ import javax.sql.DataSource
 /**
  * @author anruence@gmail.com
  */
-class TiDBEventStore : JDBCEventStore {
-    constructor(dataSource: DataSource, eventSerializer: IEventSerializer, serializeService: ISerializeService) : super(dataSource, eventSerializer, serializeService) {}
-    constructor(dataSource: DataSource, setting: DBConfiguration, eventSerializer: IEventSerializer, serializeService: ISerializeService) : super(dataSource, setting, eventSerializer, serializeService) {}
+class TiDBEventStore(dataSource: DataSource, setting: DBConfiguration, eventSerializer: IEventSerializer, serializeService: ISerializeService) : JDBCEventStore(dataSource, setting, eventSerializer, serializeService) {
 
     public override fun parseDuplicateCommandId(msg: String): String {
         val matcher = PATTERN_MYSQL.matcher(msg)
